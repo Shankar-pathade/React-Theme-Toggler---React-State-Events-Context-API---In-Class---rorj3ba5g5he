73e5209c6951b4c68ed59b9e79ac3d6dfe80a94d
Shankar-pathade <109196454+Shankar-pathade@users.noreply.github.com>
2023-03-21 14:46:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
-    <div id="main" className={isDarkMode?'dark':'light'}>
+    <div id="main" className={isDarkMode?:'light':'dark'}>
       <h1>{isDarkMode ? "Dark Mode Activated":"Light Mode Activated"}</h1>
       <button onClick={() =>{setIsDarkMode(!isDarkMode)}}>{isDarkMode ?'Switch to Light Mode':'Switch to Dark Mode'}</button>
     </div>
